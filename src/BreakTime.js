b0882c7a52b11d3d7b7485c4f09c04309511f8ea
RachelKoldenhoven <rachel@koldenfrozen.com>
2017-12-13 14:52:12 +0100

    Add photo credit and correct page height

--- src/BreakTime.js
+++ src/BreakTime.js
@@ -8,7 +8,7 @@ class BreakTime extends Component {
             <p className="CompLabel">BreakTime</p>
             <p>Time for a break!  Go grab a coffee.</p>
             <div className="BreakImg">
-                <img className="Coffee" src={require("./drew-coffman-1876.jpg")} alt="cup of cappuccino"/>
+                <img className="Coffee" src={require("./drew-coffman-1876.jpg")} alt="cup of cappuccino by Drew Coffman on unsplash"/>
                 <button onClick={this.props.switchView}>Back To Work</button>            
             </div>
 
