ba6af886e98717df7a43fad06c17a0570f141586
Teddi Maull <teddimaull@gmail.com>
2019-09-16 13:17:08 -0600

    Add Todo component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Todo from './Todo';
 
 class TodoList extends Component {
     constructor(props){
@@ -19,7 +20,7 @@ class TodoList extends Component {
               <p className="CompLabel">TodoList</p>
               <h5>To Do:</h5>
               <ol>
-                {this.props.todos.map((todo,i) => <li key={i}>{todo.title}</li>)}
+                {this.props.todos.map((todo,i) => <Todo key={i} title={todo.title}/>)}
               </ol>
               <div className="TodoBtns">
                 <form onSubmit={this.submit}>
